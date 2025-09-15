711f5096fdf57f8d7474c4a00b0531df7eca6e4e
Iman998 <66160378+Iman998@users.noreply.github.com>
2025-09-15 14:11:53 +0330

    Add edge case tests for chrF metric

--- tests/test_chrf.py
+++ tests/test_chrf.py
@@ -14,3 +14,22 @@ def test_chrf_identical():
 
 def test_chrf_different():
     assert chrf.compute(["abc"], ["xyz"]) == 0.0
+
+def test_chrf_empty_lists():
+    assert chrf.compute([], []) == 0.0
+
+
+def test_chrf_unequal_lengths_overlapping():
+    assert chrf.compute(["abcdef", "xyz"], ["abcdef"]) == 1.0
+
+
+def test_chrf_non_string_inputs():
+    assert chrf.compute([123456], [123456]) == 1.0
+    assert chrf.compute([123456], [789000]) == 0.0
+
+
+def test_chrf_empty_prediction_or_reference():
+    assert chrf.compute(["abc"], []) == 0.0
+    assert chrf.compute([], ["abc"]) == 0.0
+    assert chrf.compute([""], ["abc"]) == 0.0
+    assert chrf.compute(["abc"], [""]) == 0.0
